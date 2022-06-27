b3cb494cf4a05822e27ad8cb2d4f1840a77e29a9
jithinthomas0011 <34642677+jithinthomas0011@users.noreply.github.com>
2022-06-27 08:40:52 +0530

    Create const_cast_ex1.cpp

---
+++ casting/const_cast_ex1.cpp
@@ -0,0 +1,28 @@
+/*const_cast Example*/
+
+#include<iostream>
+using namespace std;
+
+class Base{
+
+	private :
+		int i;
+	public:
+		Base(int i_=0):i(i_){cout<<__func__<<endl;}
+		int get()const{return i;}
+		void set(int i_=0){i=i_;}
+		void Display()const{cout<<"i ="<<i<<endl;}
+
+};
+
+int main(){
+	const Base b(100);
+	cout<<b.get()<<endl;
+
+	//b.set(200); //should give error. const object cant call non const member functions
+	const_cast<Base &>(b).set(200); //Creates a non const reference to 'b' object.
+	cout<<b.get()<<endl;
+	cin.get();
+	return 0;
+
+}
